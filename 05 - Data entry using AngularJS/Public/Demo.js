00b19f4e3f439ddc5ddd4c2879be970f9bd3ea49
Maurice de Beijer <maurice.de.beijer@gmail.com>
2014-07-29 13:42:17 +0100

    05 - Data entry using AngularJS

---
+++ 05 - Data entry using AngularJS/Public/Demo.js
@@ -0,0 +1,21 @@
+﻿(function() {
+    var mod = angular.module("booksApp", []);
+
+    mod.controller("booksCtrl", function($scope) {
+
+        $scope.isBookRequired = true;
+
+        $scope.book = {
+            title: "The Angular book",
+            author: "Someone",
+            year: 2014
+        };
+
+        $scope.save = function () {
+            console.log($scope.form);
+            alert("Saving the book with title: " + $scope.book.title);
+        };
+    });
+
+
+}());
