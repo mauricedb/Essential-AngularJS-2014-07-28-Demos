04952c64073ba86bcdc56af3c3c7993551f097e8
Maurice de Beijer <maurice.de.beijer@gmail.com>
2014-07-30 16:44:42 +0100

    09 - AngularJS Patterns

---
+++ 09 - AngularJS Patterns/Public/App/App.js
@@ -0,0 +1,25 @@
+﻿/// <reference path="../../Scripts/angular.js" />
+
+(function(angular) {
+    "use strict";
+    var app = angular.module("moviesApp", []);
+
+    app.controller("MoviesCtrl", ["$scope",
+        function($scope) {
+            $scope.movies = movies;
+            $scope.directors = directors;
+            $scope.countries = countries;
+
+            $scope.selectedMovie = null;
+
+            $scope.select = function(movie) {
+                $scope.selectedMovie = movie;
+            };
+
+            $scope.save = function() {
+                $scope.selectedMovie = null;
+            };
+        }
+    ]);
+
+}(angular));
