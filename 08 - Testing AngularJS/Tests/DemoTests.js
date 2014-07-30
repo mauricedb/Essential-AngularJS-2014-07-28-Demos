70ca1e860989e9c233f83ab581d81d1e4f4f807c
Maurice de Beijer <maurice.de.beijer@gmail.com>
2014-07-30 15:08:06 +0100

    08 - Testing AngularJS

---
+++ 08 - Testing AngularJS/Tests/DemoTests.js
@@ -0,0 +1,40 @@
+﻿describe("The unit", function () {
+
+    it("should pass", function () {
+
+        expect(false).not.toBe(true);
+    });
+
+})
+
+
+describe("The controller", function () {
+    var controller, scope;
+
+    beforeEach(module("moviesApp"));
+
+    beforeEach(inject(function ($controller) {
+        scope = {};
+
+        var moviesSvc = {
+            query:function() {
+                return [{}];
+            }
+        };
+
+        controller = $controller("moviesCtrl", {
+            $scope: scope,
+            moviesSvc: moviesSvc
+        });
+
+    }));
+
+
+    it("should be created", function() {
+        expect(controller).toBeDefined();
+    });
+
+    it("should contain movies", function() {
+        expect(scope.movies.length).toBe(1);
+    })
+})
